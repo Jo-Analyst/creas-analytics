467306db3adccf431541e1d5d1bc3c1c567f71d4
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2025-02-06 14:39:16 -0300

    bloqueando o redimensionamento das linhas do datagridview

--- Interface/Views/User/FrmUsers.Designer.cs
+++ Interface/Views/User/FrmUsers.Designer.cs
@@ -67,6 +67,8 @@
             // 
             this.dgvUsers.AllowUserToAddRows = false;
             this.dgvUsers.AllowUserToDeleteRows = false;
+            this.dgvUsers.AllowUserToResizeColumns = false;
+            this.dgvUsers.AllowUserToResizeRows = false;
             this.dgvUsers.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
